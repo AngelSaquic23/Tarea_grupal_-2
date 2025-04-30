be33d839caa08f14ad95fe9bd7d085f194f8add0
GUYER1506 <etzan.noel15@gmail.com>
2025-04-29 23:30:26 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,5 +5,14 @@ using namespace std;
 void menuFase1();
 void menuFase2();
 
-             
+int main() {
+    int opcion;
 
+    do {
+        system("cls");
+        cout << "===== MENU PRINCIPAL =====\n";
+        cout << "1. Fase 1\n";
+        cout << "2. Fase 2\n";
+        cout << "0. Salir\n";
+        cout << "Seleccione una opcion: ";
+        cin >> opcion;
