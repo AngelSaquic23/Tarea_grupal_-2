9ef782fe7b398257367f0a4bc922d7dee9a77da8
Ludw1n <lajualipd@miumg.edu.gt>
2025-04-29 23:45:08 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -17,27 +17,15 @@ cout << "2. Fase 2\n";
         cout << "0. Salir\n";
         cout << "Seleccione una opcion: ";
         cin >> opcion;
-switch(opcion) {
-            case 1: menuFase1(); break;
-            case 2: menuFase2(); break;
 
+switch(opcion) {
+case 1: menuFase1(); break;
+case 2: menuFase2(); break;
+case 0: cout << "Saliendo...\n"; break;
+default: cout << "Opcion invalida.\n"; system("pause"); break;
 
 void menuFase1() {
     int op;
     do {
  switch(op) {
             case 1: system("start FASE1.exe"); break;
- }
-
-    } while(op != 0);
-}
-}
-
-void menuFase2() {
-    int op;
-    do {
-    	system("cls");
-    	cout << "1. Iniciar Programa\n";
-    	cout << "0. Regresar\n";
-        cout << "Seleccione una opcion: ";
-    	  cin >> op;
